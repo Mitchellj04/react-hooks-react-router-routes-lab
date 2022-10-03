c1d54efa6c14b0c61e517b3bcd5da9ff0468d96e
JustinMitchell <mitchelljm04@gmail.com>
2022-10-03 13:55:05 -0400

    Assignment Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import {BrowserRouter, Route, Switch } from "react-router-dom";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import Actors from "./Actors";
@@ -7,7 +7,21 @@ import Directors from "./Directors";
 import Movies from "./Movies";
 
 function App() {
-  return <div>{/*{code here}*/}</div>;
-}
+  return (
+  
+  <div>
+    <BrowserRouter>
+      <NavBar />
+      <Switch>
+        <Route exact path="/"> <Home/></Route>
+        <Route path="/movies"> <Movies/></Route>
+        <Route path="/directors" component={Directors} />
+        <Route path="/actors" component={Actors} />
+      </Switch>
+
+    </BrowserRouter>
+
+  </div>
+)}
 
 export default App;
